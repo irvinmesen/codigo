41ed2a9878efbb8ff3f8589f34af43c0d8f99605
ulacit <ulacit@405-13.docente.ulacit.ac.cr>
2018-10-25 19:10:13 -0600

    is is the firtst commint

---
+++ src/codigo/Codigo.java
@@ -0,0 +1,17 @@
+
+package codigo;
+
+/**
+ *
+ * @author ulacit
+ */
+public class Codigo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
